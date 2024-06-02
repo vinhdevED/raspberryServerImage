edd87f58c907a52a6ca4c32bd5f720fe994590c8
vinhdevED <tdinhvinh2002@gmail.com>
2024-06-02 21:24:33 +0700

    Update

--- app.py
+++ app.py
@@ -1,78 +1,45 @@
-# from flask import Flask, request, jsonify, render_template
-# import torch
-# import torchvision.transforms as transforms
-# from PIL import Image
-#
-# app = Flask(__name__)
-#
-# # Load pre-trained PyTorch model
-# model = torch.load("fruit_model_state_dict.pth", map_location=torch.device('cpu'))
-#
-#
-# # Define transformation to apply to incoming images
-# preprocess = transforms.Compose([
-#     transforms.Resize(256),
-#     transforms.CenterCrop(224),
-#     transforms.ToTensor(),
-#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-# ])
-#
-# # Function to preprocess image
-# def preprocess_image(image):
-#     image = preprocess(image).unsqueeze(0)
-#     return image
-#
-# # Function to predict
-# def predict(image):
-#     with torch.no_grad():
-#         outputs = model(image)
-#         _, predicted = torch.max(outputs, 1)
-#         return predicted.item()
-#
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-#
-# @app.route('/predict', methods=['POST'])
-# def prediction():
-#     if 'file' not in request.files:
-#         return jsonify({'error': 'no file provided'})
-#
-#     file = request.files['file']
-#     if not file:
-#         return jsonify({'error': 'no file provided'})
-#
-#     try:
-#         image = Image.open(file)
-#         image = preprocess_image(image)
-#         prediction = predict(image)
-#         return jsonify({'prediction': prediction})
-#     except Exception as e:
-#         return jsonify({'error': str(e)})
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
-from flask import Flask, render_template, request
-from inference import get_flower_name
+
+
+from flask import Flask, render_template, request, redirect
 
 import os
-
+from commons import pred
 app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Ensure the upload directory exists
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    if request.method == 'GET':
-        return render_template('index.html')
-    if request.method == 'POST':
-        try:
-            file = request.files['file']
-            image = file.read()
-            flower_name = get_flower_name(image_bytes=image)
-            return render_template('result.html', flower=flower_name)
-        except:
-            return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'image' not in request.files:
+        return redirect(request.url)
+    file = request.files['image']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filename = file.filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        return render_template('index.html', image_url=file_path)
+    return redirect('/')
+
+
+@app.route('/predict', methods=['POST'])
+def predict_file():
+    image_url = request.form['image_url']
+
+    label, confidence = pred(image_url)
+    return render_template('result.html', image_url=image_url, label=label, confidence=confidence)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- commons.py
+++ commons.py
@@ -4,22 +4,41 @@ import torch
 import torch.nn as nn
 from torchvision import models,transforms
 from PIL import Image
+import torchvision
+from cls.going_modular.going_modular.predictions import pred_and_plot_image
 
-
+class_names = ['covid','normal', 'viral pneumonia']
 def get_model():
-	checkpoint_path='classifier.pt'
-	model=models.densenet121(pretrained=True)
-	model.classifier=nn.Linear(1024,102)
+	device = "cuda" if torch.cuda.is_available() else "cpu"
+	checkpoint_path='vit_32b_10ep.pt'
+	model=torchvision.models.vit_b_16(weights=torchvision.models.ViT_B_16_Weights.DEFAULT).to(device)
+	# for parameter in mo.parameters():
+    # 	parameter.requires_grad = False
+    
+# 4. Change the classifier head 
+	class_names = ['covid','normal', 'viral pneumonia']
+
+
+	model.heads = nn.Linear(in_features=768, out_features=len(class_names)).to(device)
+	#model.classifier=nn.Linear(1024,102)
 	model.load_state_dict(torch.load(checkpoint_path,map_location='cpu'),strict=False)
 	model.eval()
 	return model
 
-def get_tensor(image_bytes):
-	my_transforms=transforms.Compose([transforms.Resize(255),
-		                              transforms.CenterCrop(224),
-		                              transforms.ToTensor(),
-		                              transforms.Normalize(
-		                              	[0.485,0.456,0.406],
-		                              	[0.229,0.224,0.225])])
-	image=Image.open(io.BytesIO(image_bytes))
-	return my_transforms(image).unsqueeze(0)
+
+def pred(img):
+	return pred_and_plot_image(model=get_model(), 
+					 	image_path=img,
+						 class_names=class_names )
+	
+
+
+# def get_flower_name(image_bytes):
+#     tensor=get_tensor(image_bytes)
+#     outputs=model(tensor)
+#     _,prediction=outputs.max(1)
+#     category=prediction.item()
+#     class_idx=idx_to_class[category]
+#     flower_name=cat_to_name[class_idx]
+
+#     return flower_name
